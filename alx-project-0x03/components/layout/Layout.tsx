f9c49a01e564fcf97a14cd63bbc9139c295b92bf
Shannia <shanniageofrey@gmail.com>
2025-07-27 17:05:48 +0300

    Implementing a share layout

---
+++ alx-project-0x03/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { LayoutProps } from "@/interfaces";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
